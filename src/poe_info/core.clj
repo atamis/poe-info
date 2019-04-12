92da56f8f8c0a0fe682b304747359b055b914f48
Andrew Amis <atamiser@gmail.com>
2019-04-12 14:42:34 -0700

    Added function to cache a full stash.

--- src/poe_info/core.clj
+++ src/poe_info/core.clj
@@ -10,10 +10,8 @@
             [poe-info.constants :as constants]
             [poe-info.gems :as gems]
             [poe-info.config :as config]
-            [poe-info.api :as api]
-            )
+            [poe-info.api :as api])
   (:gen-class))
-
 
 (comment
 
@@ -24,17 +22,37 @@
            :body
            (spit filename)))))
 
+(defn archive-tabs
+  [username cs]
+  (let [sanitize #(-> %
+                      (clojure.string/replace #" " "_")
+                      (clojure.string/replace #"[^a-zA-Z0-9\-\_]" ""))
+        tabs (->> (client/get (api/stash-item-url username)
+                              {:as :json :cookie-store cs})
+                  :body
+                  :tabs
+                  (map #(-> %
+                            (select-keys [:n :i])
+                            (clojure.set/rename-keys {:n :name :i :index})
+                            (update :name sanitize))))]
+    (doseq [{:keys [name index]} tabs]
+      (let [filename (format "cache/archive_%d_%s.json" index name)]
+        (clojure.java.io/make-parents filename) ; really only 1 is necessary
+        (as-> (client/get (api/stash-item-url username index) {:cookie-store cs}) $
+          (:body $)
+          (spit filename $))
+        (println filename)))))
+
 (defn histogram-identified
   "Histogram of whether items are identified or not."
   [items]
-  (->> items (map item/identified?) util/histogram)
-  )
+  (->> items (map item/identified?) util/histogram))
 
 (defn percent-full
   "Calculates the percentage of slots full in dump tabs."
   [stashes items]
   (float (/ (->> items (map item/size) (reduce +))
-                            (* (count stashes) constants/quad-stash))))
+            (* (count stashes) constants/quad-stash))))
 
 (defn- count-div-cards
   "Counts the number of divination cards"
@@ -45,8 +63,8 @@
   "Histogram of rares and whether they are identified or not."
   [items]
   (->> items (filter #(= (item/rarity %) :rare))
-                (map :identified)
-                util/histogram))
+       (map :identified)
+       util/histogram))
 
 (defn -main
   "Count how many items are in tab indexes 1, 2, 3. Loads data from config.edn."
@@ -60,7 +78,7 @@
   (def items
     (->>
      stashes
-     (map #(api/stash-item-url username %) )
+     (map #(api/stash-item-url username %))
      (map #(client/get % {:cookie-store cs :as :json}))
      (mapcat #(get-in % [:body :items]))))
 
@@ -69,8 +87,7 @@
     :identified (histogram-identified items)
     :percent-full (percent-full stashes items)
     :div-cards (count-div-cards items)
-    :identified-rares (histogram-identified-rares items)})
-  )
+    :identified-rares (histogram-identified-rares items)}))
 
 (comment
   (->> items (filter #(= (:category %) {:currency []})) (map :typeLine) util/histogram))
