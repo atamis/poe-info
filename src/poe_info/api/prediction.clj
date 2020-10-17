0b9c81d53bfa0a4c27a70b55de1f16b06f9fee46
Azrea Amis <atamiser@gmail.com>
2020-10-17 18:42:09 +0200

    Add some documentation about prediction parsing

--- src/poe_info/api/prediction.clj
+++ src/poe_info/api/prediction.clj
@@ -6,10 +6,13 @@
             [pl.danieljanus.tagsoup :as tagsoup]))
 
 (defn element?
+  "Is a hiccup/tagsoup element?"
   [e]
   (keyword? (first e)))
 
 (defn filter-els
+  "Filter a list of elements or the children of a single element based on a
+  predicate."
   [f e?]
   (let [elements (if (element? e?)
                    (tagsoup/children e?)
@@ -17,57 +20,84 @@
     (filter f elements)))
 
 (defn filter-el
+  "Filter a list of elements or the children of a single element based on a
+  predicate. If it returns more than one element, throw an exception."
   [f elements]
   (let [tag? (filter-els f elements)]
     (assert (>= 1 (count tag?)))
     (first tag?)))
 
 (defn get-id
-  "IDs are strings"
+  "Get an element from a list of elements or the children of an element by HTML
+  ID. IDs are strings. Throw an exception if you find more than one matching
+  element."
   [id elements]
   (filter-el #(= id (:id (tagsoup/attributes %))) elements))
 
 (defn get-tags
+  "Get an element from a list of elements or the children of an element by HTML
+  tag. Tags are keywords."
   [tag elements]
   (filter-els #(= tag (tagsoup/tag %)) elements))
 
 (defn get-tag
+  "Get an element from a list of elements or the children of an element by HTML
+  tag. Tags are keywords. Throw an exception of you find more than one matching
+  element."
   [tag elements]
   (filter-el #(= tag (tagsoup/tag %)) elements))
 
 (defn parse-tooltip
+  "Parse the first items's tooltip string from the parsed HTML."
   [html]
   (let [raw-str (->> html
+                     ;; The tooltip is stored in a Javascript string in the only
+                     ;; script tag that is in the grid.
+                     ;;
+                     ;; The grid contains the item icons and the scripts to show
+                     ;; their tooltips, and there should be only one item
+                     ;; and one script.
                      (get-tag :body)
                      (get-tag :form)
                      (get-id "grid")
                      (get-tag :script)
-                     (tagsoup/children )
+                     (tagsoup/children)
                      first
+                     ;; Regex for a string. There should be only one, so we
+                     ;; take the first one.
                      (re-seq #"\"([^\"])+\"")
                      first
                      first
                      )]
+    ;; String has some extraneous characters because the regex isn't very well
+    ;; made.
     (subs raw-str 1 (- (count raw-str) 1))))
 
 (defn tooltip->price-table
   [tooltip-str]
+  ;; Tooltip is a string of html.
   (->> tooltip-str
        (tagsoup/parse-string)
+       ;; The string doesn't actually have a body, TagSoup adds one.
        (get-tag :body)
        (get-tags :p)
        last
        (get-tag :table)))
 
 (defn parse-price-paragraph
-    [p]
-    (let [children (tagsoup/children p)
-          price (nth children 2)
-          currency (nth children 4)]
-      [:price (edn/read-string price) (keyword currency)]))
+  ;; These are the paragraphs that contain the actual
+  ;; min and max price.
+  [p]
+  (let [children (tagsoup/children p)
+        price (nth children 2)
+        currency (nth children 4)]
+    [:price (edn/read-string price) (keyword currency)]))
 
 (defn parse-response
   [resp]
+  ;; Min is in the first TD, max is in the second TD with some other stuff. The
+  ;; pricing info is in the first paragraph tag (the max TD has some additional
+  ;; paragraphs that don't matter).
   (let [table (tooltip->price-table (parse-tooltip (tagsoup/parse (:body resp))))
 
         tds (get-tags :td (get-tag :tr table))
